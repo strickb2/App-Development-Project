71f3f675c4bfcf17f4a06b922a4ab3bc1fbd299e
Aaron Nolan <aaron.nolan67@mail.dcu.ie>
2021-11-15 15:27:38 +0000

    store page urls set

--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -6,12 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Store' });
 });
 
+router.get('/product', function(req, res, next) {
+  res.render('product', { title: 'Store | Product' });
+});
+
 router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Store | Login' });
 });
 
 router.get('/signup', function(req, res, next) {
   res.render('signup', { title: 'Store | Sign Up' });
+});
+
+router.get('/cart', function(req, res, next) {
+  res.render('cart', { title: 'Store | Cart' });
+});
+
+router.get('/order', function(req, res, next) {
+  res.render('order', { title: 'Store | Order' });
 });
 
 module.exports = router;
